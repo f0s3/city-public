4027383217239a6b402ea4a8fccabdc5ee580512
TarasTurchenko <TurchTas.1551@gmail.com>
2018-05-19 17:40:38 +0300

    new design to page

--- city-public-frontend/src/app/home/home.component.ts
+++ city-public-frontend/src/app/home/home.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {filter, map} from "rxjs/operators";
-import {PostService} from "./services/post.service";
-
-class Location {
-  country: string;
-  city: string;
-}
 
 @Component({
   selector: 'app-home',
@@ -14,6 +7,13 @@ class Location {
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  cityForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.cityForm = fb.group({
+      city: ''
+    })
+  }
 
   ngOnInit() {
   }
