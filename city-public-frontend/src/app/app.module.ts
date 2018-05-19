b5eca7afb04b14e626aa2146550093fe076a3a7d
TarasTurchenko <TurchTas.1551@gmail.com>
2018-05-19 11:19:46 +0300

    Added routing

--- city-public-frontend/src/app/app.module.ts
+++ city-public-frontend/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./app-routes";
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
