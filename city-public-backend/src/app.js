51ac0a65de04b0dd341e4c355900c018129bc8d8
f0s3 <power.aerozoll@gmail.com>
2018-05-19 10:21:09 +0300

    Initialized backend.

---
+++ city-public-backend/src/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+let app = express();
+let port = 8080;
+
+app.use(morgan('combined'));
+
+app.get('/posts', (req, res) =>
+		res.send({
+			posts:[
+				{
+					user: {
+						id: 429,
+						nickname:"test123nickname",
+						photo:"user_photo_url"
+					},
+					title: "TITLE",
+					description: "long text that is our description and this is basic hello world from backend side!",
+					media:[{type:"VIDEO", url:"url1"}, {type:"VIDEO", url:"url1"}],
+					time: "current time"
+				},
+				{
+					user: {
+						id: 429,
+						nickname:"test123nickname",
+						photo:"user_photo_url"
+					},
+					title: "TITLE",
+					description: "long text that is our description and this is basic hello world from backend side!",
+					media:[{type:"VIDEO", url:"url1"}, {type:"VIDEO", url:"url1"}],
+					time: "current time"
+				}
+			]
+		})
+	);
+// app.post('/', (req, res) => {
+// 	res.send({
+// 		body:data
+// 	});
+// });
+app.listen(port, () => console.log('App listening on port '+ port +'!'));
